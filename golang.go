6c01ea5a2c62983dab333d914895a3cd9dd36f31
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2020-09-05 19:38:33 +0200

    Recursion

--- golang.go
+++ golang.go
@@ -2,48 +2,19 @@ package main
 
 import "fmt"
 
-func intSeq() func() int {
+func fact(n int) int {
 	/**
-	 * ==============================================================
-	 * This function 'intSeq' returns another function, which we
-	 * define anonymously in the body of 'intSeq'. The returned
-	 * function closes over the variable i to from a closure.
-	 * ==============================================================
+	 * ============================================================
+	 * This 'fact' function calls itself until it reaches the base
+	 * case of 'fact(0)'
+	 * ============================================================
 	 */
-	var i int = 0
-	return func() int {
-		i++
-		return i
+	if n == 0 {
+		return 1
 	}
+	return n * fact(n-1)
 }
 
 func main() {
-	/**
-	 * ==============================================================
-	 * We call 'intSeq', assigning the result (a function) to nextInt.
-	 * This function value captures its own i value, which will be
-	 * updated each time we call 'nextInt'
-	 * ==============================================================
-	 */
-	nextInt := intSeq()
-
-	/**
-	 * ==============================================================
-	 * See the effect of the closure by calling 'nextInt' a few times.
-	 * ==============================================================
-	 */
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-
-	fmt.Println("=================================================")
-
-	/**
-	 * ==============================================================
-	 * To confirm that the state is unique to that particular
-	 * function, create and test a new one.
-	 * ==============================================================
-	 */
-	newInts := intSeq()
-	fmt.Println(newInts())
+	fmt.Println(fact(7))
 }
