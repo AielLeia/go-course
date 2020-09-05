f252aa2eb8d4b87c781ba0828f486438c6bc543d
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2020-09-05 13:40:48 +0200

    Switch

--- golang.go
+++ golang.go
@@ -1,55 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	/**
-	 * ==================================
-	 * Here's a basic example
-	 * ==================================
+	 * ==================================================
+	 * Here's a basic switch
+	 * ==================================================
 	 */
-	if 7%2 == 0 {
-		fmt.Println("7 is even")
-	} else {
-		fmt.Println("7 is odd")
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
 	}
 
 	fmt.Println("===================================")
 
 	/**
-	 * ==================================
-	 * You can have an 'if' statement
-	 * without an else
-	 * ==================================
+	 * ==================================================
+	 * You can use commas to separate multiple
+	 * expression in the same case statement.
+	 * We use the optional 'default' case in this example
+	 * as well.
+	 * ==================================================
 	 */
-	if 8%4 == 0 {
-		fmt.Println("8 is divisible by 4")
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
 	}
 
 	fmt.Println("===================================")
 
 	/**
-	 * ==================================
-	 * A statement can precede
-	 * conditionals; any variables
-	 * declared in this statement are
-	 * available in all branch
-	 * ==================================
+	 * ==================================================
+	 * 'switch' without an expression is an alternate
+	 * way to express if/else logic.
+	 * Here we also show the 'case" expressions can
+	 * be non-contante
+	 * ==================================================
 	 */
-	if num := 9; num < 0 {
-		fmt.Println(num, "is negative")
-	} else if num < 10 {
-		fmt.Println(num, "has 1 digit")
-	} else {
-		fmt.Println(num, "has multiple digits")
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
 	}
 
+	fmt.Println("===================================")
+
 	/**
-	 * ==================================
-	 * Note that you don't need
-	 * parentheses around confitions in
-	 * Go, but that the braces are
-	 * required
-	 * ==================================
+	 * ==================================================
+	 * A type 'switch' compare types instead of values.
+	 * You can use this to discover the type of an
+	 * interface value. In this example, the variable
+	 * t will have type corresponding to its clause.
+	 * ==================================================
 	 */
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
