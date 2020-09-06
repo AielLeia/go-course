7bd7c619160a2e1863eb69c2c4a89066d21a1567
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2020-09-06 13:39:34 +0200

    Range over channels

--- golang.go
+++ golang.go
@@ -4,65 +4,31 @@ import "fmt"
 
 func main() {
 	/**
-	 * ==========================================================
-	 * Closing a channel incdicates that no more value will be
-	 * send ont it. This can be useful to communicate completion
-	 * to the channel's receivers.
-	 * ==========================================================
+	 * ===============================================================
+	 * In a previous example we saw how for and range provide
+	 * iteration over basic data structures. We ca, also use this
+	 * syntax to iterate over values recieved from a channel.
+	 * ===============================================================
 	 */
 
 	/**
-	 * ==========================================================
-	 * In this example we'll use a jobs channelsto communicate
-	 * work to be done from the main() goroutine to a worker
-	 * goroutine. When we have no more jobs for the worker
-	 * we'll close the jobs channels.
-	 * ==========================================================
+	 * ===============================================================
+	 * We'll iterate over 2 values in the queue channel.
+	 * ===============================================================
 	 */
-	jobs := make(chan int, 5)
-	done := make(chan bool)
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+	close(queue)
 
 	/**
-	 * ==========================================================
-	 * Here's the worker goroutine. It repeadtedly receives from
-	 * jobs with j, more := <-jobs. In this special 2-value from
-	 * of receive, the more value will be false if jobs has been
-	 * closed and all value in the channel have already been
-	 * received. We use this to notify on done when we've worked
-	 * all our jobs.
-	 * ==========================================================
+	 * ===============================================================
+	 * This range iterates over each element as it's received from
+	 * queue. Because we closed the channel abose, the iteration
+	 * terminates after receiving the 2 elements.
+	 * ===============================================================
 	 */
-	go func() {
-		for {
-			j, more := <-jobs
-			if more {
-				fmt.Println("received job", j)
-			} else {
-				fmt.Println("received all jobs")
-				done <- true
-				return
-			}
-		}
-	}()
-
-	/**
-	 * ==========================================================
-	 * This sends 3 jobs to the worker over the jobs channel,
-	 * then closes it.
-	 * ==========================================================
-	 */
-	for j := 1; j <= 3; j++ {
-		jobs <- j
-		fmt.Println("sent job", j)
+	for elem := range queue {
+		fmt.Println(elem)
 	}
-	close(jobs)
-	fmt.Println("sent all jobs")
-
-	/**
-	 * ==========================================================
-	 * We awiat the worker using the synchronization approach
-	 * we saw earlier.
-	 * ==========================================================
-	 */
-	<-done
 }
